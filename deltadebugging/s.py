c1eb0f517b25470ad089356e9efbe8a0c630fc2e
Kristina • <kristina@MacBook-Air-Kristina-2.local>
2023-04-23 20:41:09 +0200

    delta debugging

--- deltadebugging/s.py
+++ deltadebugging/s.py
@@ -1,17 +1,22 @@
 import sys
 import subprocess
 
-from yinyang.src.parsing.Parse import parse_file
+sys.path.append("/Users/kristina/Desktop/yinyang/")
+
+
 from ASTtoAPI import ASTtoAPI
+from yinyang.src.parsing.Parse import parse_file
 
 args = sys.argv
 seed = parse_file(args[1])
-
+print(seed[0])
 # CLI output
 echo_cli = subprocess.Popen(['echo', str(seed[0])], stdout=subprocess.PIPE)
 z3_cli = subprocess.Popen(['z3', '-in'], stdin=echo_cli.stdout, stdout=subprocess.PIPE)
 echo_cli.stdout.close()
 output = z3_cli.communicate()[0]
+if str(output) == "b''":
+    exit(1)
 cli_result = ""
 if "unsat" in str(output):
     cli_result = "unsat"
@@ -23,6 +28,6 @@ solver = ASTtoAPI.get_solver(seed[0])
 api_result = str(solver.check())
 
 # Check if outcomes differ
-if cli_result != api_result:
+if (cli_result == "sat" and api_result == "unsat") or (cli_result == "unsat" and api_result == "sat"):
     exit(0)
 exit(1)
--- deltadebugging/script.sh
+++ deltadebugging/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # execute the Python script and save the exit code
-python3 /Users/kristina/Desktop/playgroud/s.py $1
+python3 /Users/kristina/Desktop/yinyang/deltadebugging/s.py $1
 exit_code=$?
 
 # exit the Bash script with the same exit code as the Python script
