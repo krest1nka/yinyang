12195788c02dfe78c6c30c684b4c42f76b567360
Kristina • <kristina@MacBook-Air-Kristina-2.local>
2023-04-24 12:22:37 +0200

    script fixed

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ For fuzzing:
 
 For delta debugging: 
 
+**It is important to alter s.py path in script.sh**
+
 picireny --input phi1.smt2 --test script.sh -g SMTLIBv2.g4 --start start
--- deltadebugging/s.py
+++ deltadebugging/s.py
@@ -4,12 +4,11 @@ import subprocess
 sys.path.append("/Users/kristina/Desktop/yinyang/")
 
 
-from ASTtoAPI import ASTtoAPI
+from ASTtoAPI import ASTtoAPI, ASTtoAPIException
 from yinyang.src.parsing.Parse import parse_file
 
 args = sys.argv
 seed = parse_file(args[1])
-print(seed[0])
 # CLI output
 echo_cli = subprocess.Popen(['echo', str(seed[0])], stdout=subprocess.PIPE)
 z3_cli = subprocess.Popen(['z3', '-in'], stdin=echo_cli.stdout, stdout=subprocess.PIPE)
@@ -24,8 +23,12 @@ elif "sat" in str(output):
     cli_result = "sat"
 
 # API output
-solver = ASTtoAPI.get_solver(seed[0])
-api_result = str(solver.check())
+try:
+    solver = ASTtoAPI.get_solver(seed[0])
+    api_result = str(solver.check())
+except Exception as e:
+    print(e)
+    exit(1)
 
 # Check if outcomes differ
 if (cli_result == "sat" and api_result == "unsat") or (cli_result == "unsat" and api_result == "sat"):
