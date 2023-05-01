6fce46ffa8f2923dea2dc88568cfbd4a7668696f
Kristina • <kristina@MacBook-Air-Kristina-2.local>
2023-05-01 20:09:22 +0200

    .

--- APItoCLIprocess.py
+++ APItoCLIprocess.py
@@ -12,19 +12,19 @@ mutant = mutant[0]
 logs = open(args[1] + "/logs.txt", "a")
 elogs = open(args[1] + "/elogs.txt", "a")
 
-# try:
-solver = ASTtoAPI.get_solver(mutant)
-api_result = str(solver.check())
-print(api_result)
-# except ASTtoAPIException as error:
-#     logs.write("----------------------------\n")
-#     logs.write(str(mutant))
-#     logs.write("\n" + str(error) + "\n")
-#     logs.write("----------------------------\n")
-#     print("exception")
-# except Exception as e:
-#     elogs.write("----------------------------\n")
-#     elogs.write(str(mutant))
-#     elogs.write("\n" + str(e) + "\n")
-#     elogs.write("----------------------------\n")
-#     print("exception")
+try:
+    solver = ASTtoAPI.get_solver(mutant)
+    api_result = str(solver.check())
+    print(api_result)
+except ASTtoAPIException as error:
+    logs.write("----------------------------\n")
+    logs.write(str(mutant))
+    logs.write("\n" + str(error) + "\n")
+    logs.write("----------------------------\n")
+    print("exception")
+except Exception as e:
+    elogs.write("----------------------------\n")
+    elogs.write(str(mutant))
+    elogs.write("\n" + str(e) + "\n")
+    elogs.write("----------------------------\n")
+    print("exception")
